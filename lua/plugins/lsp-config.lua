33744384e39958c79713595a2754eab20693281b
Chris Sommer <chris.sommer@dataforcetech.com>
2024-05-04 19:51:56 -0400

    Add powershell to LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,14 +2,16 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup()
+      require("mason").setup({
+        PATH = "prepend"
+      })
     end
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "powershell_es" }
       })
     end
   },
@@ -18,24 +20,11 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.powershell_es.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
     end
   }
 }
