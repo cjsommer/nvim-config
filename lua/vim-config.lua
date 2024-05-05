1367303cd63c58e89132e563564ba1234af1b9e0
Chris Sommer <chris.sommer@dataforcetech.com>
2024-05-05 13:48:00 -0400

    Added vim logging config

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -4,3 +4,6 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 vim.cmd("set mouse=")
 vim.g.mapleader = " "
+
+-- Levels by name: "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF"
+vim.lsp.set_log_level("INFO")
