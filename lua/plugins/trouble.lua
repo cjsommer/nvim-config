992787e44e8116523e5d2c701ba54a3250eb5ea6
Chris Sommer <chris.sommer@dataforcetech.com>
2024-05-19 14:14:45 -0400

    Use trouble beta version 3

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,15 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  auto_open = true
- },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	branch = "dev",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+	},
+	opts = {
+		auto_open = true,
+	},
 }
