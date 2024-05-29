549b5cb4b71a5cc294f201e25605f76997202502
Chris Sommer <cjsommer@gmail.com>
2024-05-29 17:55:07 -0400

    Revert "Vim hard mode. Disable arrows."
    This reverts commit a584e5c26282bc87d39a37d7c131f8d9ca298eeb.

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -3,27 +3,8 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set mouse=")
+vim.cmd("set number")
 vim.cmd("set relativenumber")
-
-vim.cmd("nnoremap <Up> <Nop>")
-vim.cmd("nnoremap <Down> <Nop>")
-vim.cmd("nnoremap <Left> <Nop>")
-vim.cmd("nnoremap <Right> <Nop>")
-
-vim.cmd("inoremap <Up> <Nop>")
-vim.cmd("inoremap <Down> <Nop>")
-vim.cmd("inoremap <Left> <Nop>")
-vim.cmd("inoremap <Right> <Nop>")
-
-vim.cmd("cnoremap <Up> <Nop>")
-vim.cmd("cnoremap <Down> <Nop>")
-vim.cmd("cnoremap <Left> <Nop>")
-vim.cmd("cnoremap <Right> <Nop>")
-
-vim.cmd("vnoremap <Up> <Nop>")
-vim.cmd("vnoremap <Down> <Nop>")
-vim.cmd("vnoremap <Left> <Nop>")
-vim.cmd("vnoremap <Right> <Nop>")
 
 vim.g.mapleader = " "
 
